2d41756b8d734463f37b062f77eeb3433a4959c8
roberto angioletti <you@example.com>
2024-05-14 10:40:48 +0200

    now showeing map again

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,37 @@
+// Map.js
 import React, { useState, memo } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const StateMarker = memo ({ state, selectedState, handleCityGuess, stateIcon }) ; {
- const shouldShowStateName = selectedState === (state) || (selectedState && handleCityGuess(state, selectedState.capital) === true);
+const shouldShowStateName = (state, selectedState, handleCityGuess) => {
+  return selectedState === state || (selectedState && handleCityGuess(state, selectedState.capital) === true);
+};
 
-
+const StateMarker = memo(({ state, selectedState, handleCityGuess, stateIcon, shouldShowStateNameFunc }) => {
+  return (
+    <Marker
+      key={state.id}
+      position={[state.latitude, state.longitude]}
+      icon={stateIcon}
+    >
+      <Popup>
+        {shouldShowStateNameFunc(state, selectedState, handleCityGuess) ? (
+          <div>
+            <h3>{state.name}</h3>
+            <p>Capital: {state.capital}</p>
+          </div>
+        ) : (
+          <p>Guess the state</p>
+        )}
+      </Popup>
+    </Marker>
+  );
+}, (prevProps, nextProps) => {
+  return prevProps.state.id === nextProps.state.id &&
+         prevProps.selectedState === nextProps.selectedState &&
+         prevProps.handleCityGuess === nextProps.handleCityGuess &&
+         prevProps.stateIcon === nextProps.stateIcon &&
+         prevProps.shouldShowStateNameFunc === nextProps.shouldShowStateNameFunc;
+});
 
 const Map = ({ stateData, selectedState, onStateClick, stateIcon, onCityGuess, showSolution }) => {
   const [guessedCity, setGuessedCity] = useState('');
@@ -14,34 +41,21 @@ const Map = ({ stateData, selectedState, onStateClick, stateIcon, onCityGuess, s
     setGuessedCity('');
   };
 
-  const shouldShowStateName = (state) => {
-    return state === selectedState || handleCityGuess(state, state.capital) === true;
-  };
-
   return (
-    <MapContainer center={[37.0902, -95.7129]} 
-        zoom={4}  
-        scrollWheelZoom={false} 
-        style={{ height: '80vh', width: '100%' }}>
-
-
+    <MapContainer center={[37.0902, -95.7129]} zoom={4} scrollWheelZoom={false} style={{ height: '80vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
       />
-    <Marker
-    key={state.id}
-    position={[state.latitude,state.longitude]}
-    icon={stateIcon}
-    ></Marker>
-    
+
       {stateData.map((state) => (
         <StateMarker
           key={state.id}
           state={state}
-          position={[state.latitude, state.longitude]}
+          selectedState={selectedState}
           handleCityGuess={handleCityGuess}
           stateIcon={stateIcon}
+          shouldShowStateNameFunc={shouldShowStateName}
           eventHandlers={{
             click: () => onStateClick(state),
           }}
@@ -50,14 +64,6 @@ const Map = ({ stateData, selectedState, onStateClick, stateIcon, onCityGuess, s
             <h3>{state.name}</h3>
             <p>Capital: {state.capital}</p>
 
-            {shouldShowStateName? (
-              <div>
-                <h3>{state.name}</h3>
-                <p>Capital: {state.capital}</p>
-                </div>
-            ) : (
-              <p>Guess the state</p>
-            )}
             <input
               type="text"
               value={guessedCity}
@@ -67,14 +73,10 @@ const Map = ({ stateData, selectedState, onStateClick, stateIcon, onCityGuess, s
             <button onClick={() => handleCityGuess(state)}>Guess</button>
             <button onClick={() => showSolution(state.capital)}>See solution</button>
           </Popup>
-        </StateMarker> 
-       ) 
-       )}
-    
+        </StateMarker>
+      ))}
     </MapContainer>
   );
-   }
- }; 
-  
+};
 
-export default Map
+export default Map;
